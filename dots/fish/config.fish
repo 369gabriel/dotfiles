52edbc04e0f1c59b64ba95b12a0d7429f5959fa4
gabriel gyaf <villedwhite@gmail.com>
2025-03-03 10:15:45 -0300

    updating

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -2,9 +2,9 @@ if status is-interactive
     alias s 'sudo'
     alias p 'pacman'
     alias jbl 'bluetoothctl connect 70:99:1C:A6:9E:BA'
-    alias fone 'bluetoothctl connect B7:5B:24:E3:18:D7'
+    alias fone 'bluetoothctl connect 41:42:27:90:00:00'
     alias djbl 'bluetoothctl disconnect 70:99:1C:A6:9E:BA'
-    alias dfone 'bluetoothctl disconnect B7:5B:24:E3:18:D7'
+    alias dfone 'bluetoothctl disconnect 41:42:27:90:00:00'
     
     alias files 'ranger'
     alias ..="cd .." 
@@ -16,15 +16,21 @@ if status is-interactive
     alias weather='curl wttr.in'
     alias volup='pulsemixer --change-volume +5'
     alias voldown='pulsemixer --change-volume -5'
+    alias nvimconfig='nvim ~/.config/nvim'
+    alias dots='nvim ~/.config'
     set CONFIG ~/.config
     starship init fish | source
 
 set -U fish_greeting
 set -x XDG_CONFIG_HOME $HOME/.config
 export EDITOR=nvim
-
 end
 fish_add_path ~/.spicetify
 if test -z "$DISPLAY" -a (tty) = "/dev/tty1"
     startx
+end
+
+if command -v tmux &> /dev/null && [ -z "$TMUX" ]
+    # Tenta anexar a uma sessão existente
+    tmux attach || tmux new
 end
--- dots/nvim/init.lua
+++ dots/nvim/init.lua
@@ -1,22 +1,62 @@
--- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
--- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
-local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+require("gyaf")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
--- validate that lazy is available
-if not pcall(require, "lazy") then
-  -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
-  vim.fn.getchar()
-  vim.cmd.quit()
-end
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+  
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+      {
+          'nvim-telescope/telescope.nvim',
+          version = '0.1.8', -- ou use `branch = '0.1.x'`
+          dependencies = { 'nvim-lua/plenary.nvim' },
+      },
+      {
+      "dgox16/oldworld.nvim",
+      lazy = false,
+      priority = 1000,
+      },
+      'nvim-treesitter/nvim-treesitter',
+      'theprimeagen/harpoon',
+      'mbbill/undotree',
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/nvim-cmp',
+      'vonheikemen/lsp-zero.nvim',
+      'm4xshen/autoclose.nvim',
+      {
+          'MeanderingProgrammer/render-markdown.nvim',
+          dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+          ---@module 'render-markdown'
+          ---@type render.md.UserConfig
+          opts = {},
+      },
+      {'akinsho/toggleterm.nvim', version = "*", config = true},
 
-require "lazy_setup"
-require "polish"
 
-vim.opt.wrap = true
-vim.opt.linebreak = true
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  -- automatically check for plugin updates
+  checker = { enabled = false },
+})
+
